829c8f208703ed789fc3fb7268a5bb88e7f1e674
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-08-23 09:00:43 +0200

    A test for output processors

---
+++ test5.py
@@ -0,0 +1,20 @@
+import snactor.executors
+from snactor.loader import load
+from snactor.registry import get_actor
+from pprint import pprint
+import logging
+
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+load('examples/actors')
+
+data = {}
+print "Before execution"
+print "======================================================================="
+pprint(data)
+print "======================================================================="
+print "Execution result:", get_actor('iplist')().execute(data)
+print "======================================================================="
+print "After execution"
+pprint(data)
+
