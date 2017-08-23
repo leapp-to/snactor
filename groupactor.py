e3a7d2d920a7365aa279d7302d3332d32ea2df95
Arthur Mello <amello@redhat.com>
2017-08-23 15:37:56 -0300

    Add sample actor/script to test group executor

---
+++ groupactor.py
@@ -0,0 +1,19 @@
+""" Sample script to test group executor """
+import logging
+from pprint import pprint
+from snactor.loader import load
+from snactor.registry import get_actor
+
+
+def test_group_actor():
+    """ Execute group-actor to test group executor """
+    data = {}
+    get_actor('group-actor')().execute(data)
+    pprint(data)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
+    load('examples/actors')
+    test_group_actor()
