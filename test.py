431d9245165374abfd3c3c9dfd92fcce71fc73e2
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-08-24 14:40:10 +0200

    Fix test.py execution

--- test.py
+++ test.py
@@ -1,16 +1,19 @@
+""" Sample script to test simple executor """
 import logging
-import yaml
-from snactor.executors.default import Executor, Definition
-import sys
 from pprint import pprint
+from snactor.loader import load
+from snactor.registry import get_actor
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-a = yaml.load(open('examples/actors/simple-actor.yaml'))
-d = Definition('simple-actor', a)
-d.executor = Executor.Definition(d.executor)
-e = Executor(d)
-data = {}
-result = e.execute(data)
 
-print result
-pprint(data)
+def test_simple_actor():
+    """ Execute simple-actor to test simple executor """
+    data = {}
+    get_actor('simple-actor')().execute(data)
+    pprint(data)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
+    load('examples/actors')
+    test_simple_actor()
