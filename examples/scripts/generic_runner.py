7dcd9a65dd084e3965dddd4d76430391e99d11eb
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-08-31 13:00:54 +0200

    pep8: Do not fail on unused imports or nonstandard import locations

--- examples/scripts/generic_runner.py
+++ examples/scripts/generic_runner.py
@@ -1,17 +1,18 @@
 import logging
 import os.path
 import sys
-from pprint import pprint
+from pprint import pprint  # noqa
 
 _BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(_BASE_DIR))
 
-from snactor.loader import load, load_schemas, validate_actor_types, get_actor
+from snactor.loader import load, load_schemas, validate_actor_types, get_actor  # noqa
 
-def run(function, tags=()):
+
+def run(fun, tags=()):
     logging.basicConfig(format='%(levelname)s:%(message)s',
                         level=logging.DEBUG)
     load(os.path.join(_BASE_DIR, 'actors'), tags=tags)
     load_schemas(os.path.join(_BASE_DIR, 'schema'))
     validate_actor_types()
-    function()
+    fun()
