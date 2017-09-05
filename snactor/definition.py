71e1c78a6dfdaf5b2d20516ef7b2217d3241afc6
Pavel Cahyna <pcahyna@users.noreply.github.com>
2017-09-05 21:30:24 +0200

    Add tags to actor definition to allow searching loaded actors by tags.

--- snactor/definition.py
+++ snactor/definition.py
@@ -3,6 +3,7 @@ class Definition(object):
     def __init__(self, name, init=None):
         init = init or {}
         self.name = name
+        self.tags = set(init.get('tags', ()))
         self.inputs = init.get('inputs', ())
         self.outputs = init.get('outputs', ())
         if not isinstance(self.outputs, (list, tuple)):
