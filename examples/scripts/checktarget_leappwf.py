7d5ea682ea062b74bf8986cbd1bf34c2c027c799
Pavel Cahyna <pcahyna@users.noreply.github.com>
2017-09-07 16:18:07 +0200

    whitespace

--- examples/scripts/checktarget_leappwf.py
+++ examples/scripts/checktarget_leappwf.py
@@ -8,5 +8,6 @@ from generic_runner import wf_run
 def process_result(wfresult):
     pprint(wfresult['targetinfo'].pop().payload)
 
+
 if __name__ == '__main__':
     wf_run('check_target', process_result, tags=('check_target',))
